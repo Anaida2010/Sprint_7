4ba4bd414a69965dc08659db63667e369838c888
Anaida2010 <anaidka2010@yandex.ru>
2025-07-01 15:41:43 +0300

    conftest.py

--- conftest.py
+++ conftest.py
@@ -1,20 +1,18 @@
+from typing import Any
+
 import pytest
+from data import order
+from faker import Faker
 
-from helps import Courier
 
-
-# фикстура регистрации, авторизации и удаления курьера
 @pytest.fixture()
-def courier():
-    courier_create = Courier().courier_registration_in_the_system_and_get_courier_data()
-    courier_login = Courier().courier_login_in_the_system_and_get_id_courier(courier_create["data"])
-    yield courier_create
-    Courier().courier_subsequent_deletion(courier_login["id"])
+def courier_data() -> dict[str, str]:
+    fake = Faker()
+    return {'login': fake.user_name(),
+            'password': fake.password(),
+            'firstName': fake.file_name()}
 
-# фикстура регистрации, авторизации и удаления курьера
+
 @pytest.fixture()
-def courier_delete():
-    courier_create = Courier().courier_registration_in_the_system_and_get_courier_data()
-    print(courier_create['data'])
-    courier_login = Courier().courier_login_in_the_system_and_get_id_courier(courier_create["data"])
-    yield courier_login
+def order_data() -> dict[str, Any]:
+    return order.order_data
